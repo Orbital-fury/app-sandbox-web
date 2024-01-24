996c95075139f8e4a769b7bb361a6d01c5a99e12
Simon Debelmas <smonsmon38@gmail.com>
2024-01-24 13:34:09 +0100

    update breadcrumb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,71 +8,80 @@ import { ManageMachineComponent } from './components/mmm/manage-machine/manage-m
 import { UpdateMachineComponent } from './components/mmm/manage-machine/update-machine/update-machine.component';
 import { MmmHomeComponent } from './components/mmm/mmm-home/mmm-home.component';
 import { MmmComponent } from './components/mmm/mmm.component';
+import { ManagePcBuilderComponent } from './components/pc-builder/manage-pc-builder/manage-pc-builder.component';
+import { ManagePcConstraintComponent } from './components/pc-builder/manage-pc-builder/manage-pc-constraint/manage-pc-constraint.component';
+import { UpdatePcConstraintComponent } from './components/pc-builder/manage-pc-builder/manage-pc-constraint/update-pc-constraint/update-pc-constraint.component';
+import { ManagePcElementComponent } from './components/pc-builder/manage-pc-builder/manage-pc-element/manage-pc-element.component';
+import { PcElementComponent } from './components/pc-builder/manage-pc-builder/manage-pc-element/pc-element/pc-element.component';
+import { UpdatePcElementComponent } from './components/pc-builder/manage-pc-builder/manage-pc-element/update-pc-element/update-pc-element.component';
 import { PcBuilderComponent } from './components/pc-builder/pc-builder.component';
 import { CustomerComponent } from './components/test/customer/customer.component';
 import { DashboardComponent } from './components/test/dashboard/dashboard.component';
 import { ParallaxComponent } from './components/test/parallax/parallax.component';
-import { PcElementComponent } from './components/pc-builder/manage-pc-builder/manage-pc-element/pc-element/pc-element.component';
-import { ManagePcBuilderComponent } from './components/pc-builder/manage-pc-builder/manage-pc-builder.component';
-import { UpdatePcElementComponent } from './components/pc-builder/manage-pc-builder/manage-pc-element/update-pc-element/update-pc-element.component';
-import { UpdatePcConstraintComponent } from './components/pc-builder/manage-pc-builder/manage-pc-constraint/update-pc-constraint/update-pc-constraint.component';
-import { ManagePcElementComponent } from './components/pc-builder/manage-pc-builder/manage-pc-element/manage-pc-element.component';
-import { ManagePcConstraintComponent } from './components/pc-builder/manage-pc-builder/manage-pc-constraint/manage-pc-constraint.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
-  { path: 'test/parallax', component: ParallaxComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'customer/:customerId', component: CustomerComponent },
   {
-    path: 'pc-builder', data: { breadcrumb: 'PC Builder' }, children: [
-      { path: '', component: PcBuilderComponent, data: { breadcrumb: '' } },
-      { path: 'pc-elements/:elementId', component: PcElementComponent, data: { breadcrumb: 'PC element' } },
+    path: '', data: { breadcrumb: 'Home' }, children: [
+      { path: '', component: HomeComponent },
       {
-        path: 'manage-pc-builder', data: { breadcrumb: 'Admin manager' }, children: [
-          { path: '', component: ManagePcBuilderComponent },
+        path: 'test', children: [
+          { path: 'parallax', data: { breadcrumb: 'Parallax' }, component: ParallaxComponent }
+        ]
+      },
+      { path: 'dashboard', data: { breadcrumb: 'Dashboard' }, component: DashboardComponent },
+      { path: 'customer/:customerId', data: { breadcrumb: 'Customer - ' }, component: CustomerComponent },
+      {
+        path: 'pc-builder', data: { breadcrumb: 'PC Builder' }, children: [
+          { path: '', component: PcBuilderComponent },
+          { path: 'pc-elements/:elementId', data: { breadcrumb: 'PC element - ' }, component: PcElementComponent },
           {
-            path: 'pc-elements', children: [
-              { path: '', component: ManagePcElementComponent, data: { breadcrumb: 'PC elements' } },
-              { path: 'create', component: UpdatePcElementComponent, data: { breadcrumb: 'Create' } },
-              { path: 'edit/:elementId', component: UpdatePcElementComponent, data: { breadcrumb: '' } },
-            ]
-          },
-          {
-            path: 'pc-constraints', children: [
-              { path: '', component: ManagePcConstraintComponent, data: { breadcrumb: 'PC constraints' } },
-              { path: 'create', component: UpdatePcConstraintComponent, data: { breadcrumb: 'Create' } },
+            path: 'manage-pc-builder', data: { breadcrumb: 'Admin manager' }, children: [
+              { path: '', component: ManagePcBuilderComponent },
+              {
+                path: 'pc-elements', data: { breadcrumb: 'PC elements' }, children: [
+                  { path: '', component: ManagePcElementComponent },
+                  { path: 'create', component: UpdatePcElementComponent, data: { breadcrumb: 'Create' } },
+                  { path: 'edit/:elementId', component: UpdatePcElementComponent, data: { breadcrumb: ' - ' } },
+                ]
+              },
+              {
+                path: 'pc-constraints', data: { breadcrumb: 'PC constraints' }, children: [
+                  { path: '', component: ManagePcConstraintComponent },
+                  { path: 'create', component: UpdatePcConstraintComponent, data: { breadcrumb: 'Create' } },
+                ]
+              },
             ]
           },
         ]
       },
-    ]
-  },
-  {
-    path: 'mmm',
-    component: MmmComponent,
-    children: [
-      { path: '', component: MmmHomeComponent, outlet: 'mmm' },
       {
-        path: 'manage-machine',
+        path: 'mmm', data: { breadcrumb: 'Mmm' }, component: MmmComponent,
         children: [
-          { path: '', component: ManageMachineComponent },
-          { path: 'create', component: UpdateMachineComponent },
-          { path: ':machineId', component: MachineComponent },
+          { path: '', data: { breadcrumb: 'Overview' }, component: MmmHomeComponent, outlet: 'mmm' },
+          {
+            path: 'manage-machine', data: { breadcrumb: 'Manage machines' },
+            children: [
+              { path: '', component: ManageMachineComponent },
+              { path: 'create', data: { breadcrumb: 'Create' }, component: UpdateMachineComponent },
+              { path: ':machineId', data: { breadcrumb: 'Machine - ' }, component: MachineComponent },
+            ],
+            outlet: 'mmm',
+          },
+          {
+            path: 'machine-calibration',
+            data: { breadcrumb: 'Machine calibration' },
+            component: MachineCalibrationComponent,
+            outlet: 'mmm',
+          },
+          {
+            path: 'maintenance',
+            data: { breadcrumb: 'Maintenance' },
+            component: MaintenanceComponent,
+            outlet: 'mmm',
+          }
         ],
-        outlet: 'mmm',
       },
-      {
-        path: 'machine-calibration',
-        component: MachineCalibrationComponent,
-        outlet: 'mmm',
-      },
-      {
-        path: 'maintenance',
-        component: MaintenanceComponent,
-        outlet: 'mmm',
-      }
-    ],
+    ]
   },
 ];
 
--- src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BreadcrumbService } from './breadcrumb.service';
 
 @Component({
-    selector: 'nav[app-breadcrumb]',
-    templateUrl: './breadcrumb.component.html',
+  selector: 'nav[app-breadcrumb]',
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.scss'],
 })
 export class BreadcrumbComponent {
-    constructor(
-        public breadcrumbService: BreadcrumbService
-    ) {}
+  constructor(
+    public breadcrumbService: BreadcrumbService
+  ) { }
 }
