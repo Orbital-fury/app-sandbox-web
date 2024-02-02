93e18511a6794faadbe6d12e5c76e84523d7e825
Simon Debelmas <smonsmon38@gmail.com>
2024-02-02 14:51:45 +0100

    Update to put loader in app + fix a-tag

--- src/app/components/pc-builder/manage-pc-builder/manage-pc-element/manage-pc-element.component.ts
+++ src/app/components/pc-builder/manage-pc-builder/manage-pc-element/manage-pc-element.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { loadPcElements } from 'src/app/store/pc-builder/pc-elements/pc-elements.actions';
-import { selectPcElements } from 'src/app/store/pc-builder/pc-elements/pc-elements.selectors';
+import { selectLoadingPcElements, selectPcElements } from 'src/app/store/pc-builder/pc-elements/pc-elements.selectors';
 import { PcElementsState } from 'src/app/store/pc-builder/pc-elements/pc-elements.state';
 import { PcElement } from 'src/typing-pc-builder';
 import { SubSink } from 'subsink';
@@ -15,12 +15,14 @@ import { SubSink } from 'subsink';
 export class ManagePcElementComponent implements OnInit, OnDestroy {
 
   pcElements: PcElement[] = [];
+  loadingPcElements: boolean;
   private subs = new SubSink();
 
   constructor(private router: Router, private readonly pcElementStore: Store<PcElementsState>) { }
 
   ngOnInit() {
     this.subs.sink = this.pcElementStore.select(selectPcElements).subscribe(pcElements => this.pcElements = pcElements);
+    this.subs.sink = this.pcElementStore.select(selectLoadingPcElements).subscribe(loading => this.loadingPcElements = loading);
 
     this.pcElementStore.dispatch(loadPcElements());
   }
